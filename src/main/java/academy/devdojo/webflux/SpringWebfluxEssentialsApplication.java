c5ad73f1f0e7d3a9b688a5ec6b0a395cceb3469b
williamsuane <william.suane@devdojo.academy>
2020-05-25 18:39:48 +0200

    Spring Webflux Essentials 01 - Project Setup

---
+++ src/main/java/academy/devdojo/webflux/SpringWebfluxEssentialsApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxEssentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxEssentialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/academy/devdojo/webflux/SpringWebfluxEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxEssentialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
