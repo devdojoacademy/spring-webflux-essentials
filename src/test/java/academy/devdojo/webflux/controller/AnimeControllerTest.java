824197a1d19252815b9e2ae54968a1da321cc582
williamsuane <william.suane@devdojo.academy>
2020-06-03 20:30:43 +0200

    Spring Webflux Essentials 17 - Unit Test Controller pt 01

---
+++ src/test/java/academy/devdojo/webflux/controller/AnimeControllerTest.java
@@ -0,0 +1,94 @@
+package academy.devdojo.webflux.controller;
+
+import academy.devdojo.webflux.domain.Anime;
+import academy.devdojo.webflux.service.AnimeService;
+import academy.devdojo.webflux.util.AnimeCreator;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+@ExtendWith(SpringExtension.class)
+class AnimeControllerTest {
+
+    @InjectMocks
+    private AnimeController animeController;
+
+    @Mock
+    private AnimeService animeServiceMock;
+
+    private final Anime anime = AnimeCreator.createValidAnime();
+
+    @BeforeAll
+    public static void blockHoundSetup() {
+        BlockHound.install();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        BDDMockito.when(animeServiceMock.findAll())
+            .thenReturn(Flux.just(anime));
+
+        BDDMockito.when(animeServiceMock.findById(ArgumentMatchers.anyInt()))
+            .thenReturn(Mono.just(anime));
+
+//        BDDMockito.when(animeServiceMock.save(AnimeCreator.createAnimeToBeSaved()))
+//            .thenReturn(Mono.just(anime));
+//
+//        BDDMockito.when(animeServiceMock.delete(ArgumentMatchers.anyInt()))
+//            .thenReturn(Mono.empty());
+//
+//        BDDMockito.when(animeServiceMock.update(AnimeCreator.createValidAnime()))
+//            .thenReturn(Mono.empty());
+    }
+
+    @Test
+    public void blockHoundWorks() {
+        try {
+            FutureTask<?> task = new FutureTask<>(() -> {
+                Thread.sleep(0); //NOSONAR
+                return "";
+            });
+            Schedulers.parallel().schedule(task);
+
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        } catch (Exception e) {
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
+
+    @Test
+    @DisplayName("listAll returns a flux of anime")
+    public void listAll_ReturnFluxOfAnime_WhenSuccessful() {
+        StepVerifier.create(animeController.listAll())
+            .expectSubscription()
+            .expectNext(anime)
+            .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("findById returns a Mono with anime when it exists")
+    public void findById_ReturnMonoAnime_WhenSuccessful() {
+        StepVerifier.create(animeController.findById(1))
+            .expectSubscription()
+            .expectNext(anime)
+            .verifyComplete();
+    }
+
+}
