147fb86821b1a0ffe74ff804a3bbf04c3c24f799
williamsuane <william.suane@devdojo.academy>
2020-06-05 18:32:43 +0200

    Spring Webflux Essentials 22 - Webflux integration tests with WebTestClient pt 03

--- src/test/java/academy/devdojo/webflux/integration/AnimeControllerIT.java
+++ src/test/java/academy/devdojo/webflux/integration/AnimeControllerIT.java
@@ -19,8 +19,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.blockhound.BlockHound;
 import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Flux;
@@ -50,6 +52,9 @@ public class AnimeControllerIT {
             .thenReturn(Flux.just(anime));
 
         BDDMockito.when(animeRepositoryMock.findById(ArgumentMatchers.anyInt()))
+            .thenReturn(Mono.just(anime));
+
+        BDDMockito.when(animeRepositoryMock.save(AnimeCreator.createAnimeToBeSaved()))
             .thenReturn(Mono.just(anime));
 
     }
@@ -122,6 +127,38 @@ public class AnimeControllerIT {
             .expectBody()
             .jsonPath("$.status").isEqualTo(404)
             .jsonPath("$.developerMessage").isEqualTo("A ResponseStatusException Happened");
+    }
+
+    @Test
+    @DisplayName("save creates an anime when successful")
+    public void save_CreatesAnime_WhenSuccessful() {
+        Anime animeToBeSaved = AnimeCreator.createAnimeToBeSaved();
+
+        testClient
+            .post()
+            .uri("/animes")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromValue(animeToBeSaved))
+            .exchange()
+            .expectStatus().isCreated()
+            .expectBody(Anime.class)
+            .isEqualTo(anime);
+    }
+
+    @Test
+    @DisplayName("save returns mono error with bad request when name is empty")
+    public void save_ReturnsError_WhenNameIsEmpty() {
+        Anime animeToBeSaved = AnimeCreator.createAnimeToBeSaved().withName("");
+
+        testClient
+            .post()
+            .uri("/animes")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromValue(animeToBeSaved))
+            .exchange()
+            .expectStatus().isBadRequest()
+            .expectBody()
+            .jsonPath("$.status").isEqualTo(400);
 
     }
 }
