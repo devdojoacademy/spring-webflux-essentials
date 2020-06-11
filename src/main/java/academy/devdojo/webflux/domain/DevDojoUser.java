154c5efaaa39a25c9e41e87761889d825710351d
williamsuane <william.suane@devdojo.academy>
2020-06-11 20:21:05 +0200

    Spring Webflux Essentials 28 - Reactive Security R2DBC Postgres pt 01.mp4

---
+++ src/main/java/academy/devdojo/webflux/domain/DevDojoUser.java
@@ -0,0 +1,54 @@
+package academy.devdojo.webflux.domain;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("devdojo_user")
+public class DevDojoUser implements UserDetails {
+
+    @Id
+    private Integer id;
+    private String name;
+    private String username;
+    private String password;
+    private String authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(authorities.split(","))
+            .map(SimpleGrantedAuthority::new)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
